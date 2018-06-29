a648c52a5d48c2fff60510b8f812fa13f4daaf08
Чайка <mellanie335@gmail.com>
2018-06-29 18:38:14 +0900

    Append class Userslist and its test.

---
+++ UserslistDB/UsersManagement/Userslist.swift
@@ -0,0 +1,13 @@
+//
+//  Userslist.swift
+//  UserslistDB
+//
+//  Created by Чайка on 2018/06/29.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class Userslist: NSObject {
+
+}
