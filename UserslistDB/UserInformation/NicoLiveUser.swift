513adc0bd03be505d73a4c29887f362998153a83
Чайка <mellanie335@gmail.com>
2018-07-01 19:58:28 +0900

    Add class NicoLiveUser and tests file.

---
+++ UserslistDB/UserInformation/NicoLiveUser.swift
@@ -0,0 +1,13 @@
+//
+//  NicoLiveUser.swift
+//  UserslistDB
+//
+//  Created by Чайка on 2018/07/01.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class NicoLiveUser: NSObject {
+
+}
