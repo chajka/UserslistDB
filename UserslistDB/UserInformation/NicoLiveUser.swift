4726d42ffd0ee05aee48ab679bd8a7b979d33e19
Чайка <mellanie335@gmail.com>
2018-07-04 21:10:55 +0900

    constructor from entry test success.

--- UserslistDB/UserInformation/NicoLiveUser.swift
+++ UserslistDB/UserInformation/NicoLiveUser.swift
@@ -7,7 +7,88 @@
 //
 
 import Cocoa
+import DeuxCheVaux
+
+public enum Friendship {
+	case new
+	case known
+	case met
+	case metOther
+}// end enum Friendship
+
+public struct UserName {
+	let identifier:String
+	let nickname:String
+	var handle:String
+}// end struct UserName
+
+enum JSONKey {
+	enum user:String {
+		case nickname = "nickname"
+		case handle = "handle"
+		case isPremium = "isPremium"
+		case language = "Language"
+		case friendship = "known"
+		case lock = "lock"
+		case color = "color"
+		case met = "lasstMet"
+		case note = "note"
+	}
+}// end enum JSONKey
+
+extension JSONKey.user: StringEnum { }
 
 class NicoLiveUser: NSObject {
+	public let name:UserName
+	public let anonymous:Bool
+	public let isPremium:Bool
+	public let language:UserLanguage
+	public var friendship:Friendship
+	public var thumbnail:NSImage!
+	public var lock:Bool = false
+	public let lastMet:Date
+	public var color:NSColor?
+	public var backgroundColor:String? {
+		willSet(newColor) {
+			guard let hexColor = newColor, hexColor.count == 7, hexColor.prefix(1) == "#" else { return }
+			entry[JSONKey.user.color] = hexColor
+			let redStr:String = hexColor[hexColor.index(hexColor.startIndex, offsetBy: 1) ... hexColor.index(hexColor.startIndex, offsetBy: 2)].description
+			let greenStr:String = hexColor[hexColor.index(hexColor.startIndex, offsetBy: 3) ... hexColor.index(hexColor.startIndex, offsetBy: 4)].description
+			let blueStr:String = hexColor[hexColor.index(hexColor.startIndex, offsetBy: 5) ..< hexColor.endIndex].description
+			
+			let red:CGFloat = CGFloat(Int(redStr)!) / 0xff
+			let green:CGFloat = CGFloat(Int(greenStr)!) / 0xff
+			let blue:CGFloat = CGFloat(Int(blueStr)!) / 0xff
 
-}
+			color = NSColor(calibratedRed: red, green: green, blue: blue, alpha: 1.0)
+		}// end willSet
+	}// end computed property backgroundColor
+	public var note:String?
+
+	private var entry:Dictionary<String, String>
+
+	init(user:[String: String], identifier:String, anonymous:Bool, lang:UserLanguage) {
+		entry = user
+		self.anonymous = anonymous
+		language = lang
+		var username:String = entry[JSONKey.user.nickname]!
+		if anonymous { username = String(username.prefix(10)) }
+		let handlename:String = entry[JSONKey.user.handle]!
+		name = UserName(identifier: identifier, nickname: username, handle: handlename)
+		isPremium = entry[JSONKey.user.isPremium] == "yes" ? true : false
+		friendship = entry[JSONKey.user.friendship] == "yes" ? Friendship.known : Friendship.met
+		lock = entry[JSONKey.user.lock] == "yes" ? true : false
+			// update time
+		let formatter:DateFormatter = DateFormatter()
+		formatter.dateStyle = DateFormatter.Style.short
+		formatter.timeStyle = DateFormatter.Style.short
+		let lastMetString:String = entry[JSONKey.user.met] ?? formatter.string(from: Date())
+		lastMet = formatter.date(from: lastMetString)!
+		entry[JSONKey.user.met] = formatter.string(from: Date())
+			// color
+		if let colorString:String = entry[JSONKey.user.color] { self.backgroundColor = colorString }
+			// note
+		if let noteString:String = entry[JSONKey.user.note] { note = noteString }
+	}// end init from entry
+
+}// end class NicoLiveUser
--- UserslistDBTests/NicoLiveUserTests.swift
+++ UserslistDBTests/NicoLiveUserTests.swift
@@ -7,12 +7,27 @@
 //
 
 import XCTest
+import DeuxCheVaux
 
 class NicoLiveUserTests: XCTestCase {
+	var ownersDictionary:Dictionary<String, Any>?
+	var usersDictionary:Dictionary<String, Any>?
+	var user:Dictionary<String, String>?
 
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+		do {
+			let fullpath:String = "/Volumes/SharkWire/build/UserslistDB/UserslistDBTests/test.json"
+			let data:NSData = try NSData(contentsOfFile: fullpath)
+			let jsonObj:[String: [String: Any]] = try JSONSerialization.jsonObject(with: data as Data, options: [JSONSerialization.ReadingOptions.mutableContainers, JSONSerialization.ReadingOptions.mutableLeaves]) as! [String : [String : Any]]
+			ownersDictionary = jsonObj["owners"] as! [String : [String : Any]]
+			usersDictionary = jsonObj["users"] as! [String : Bool]
+			let owner:Dictionary<String, Any> = ownersDictionary!["6347612"]! as! Dictionary<String, Any>
+			let listenes:Dictionary<String, Dictionary<String, String>> = owner["listener"]! as! Dictionary<String, Dictionary>
+			user = listenes["6347612"]!
+		} catch {
+			print(error)
+		}// end try - catch open data and parse json to dictionary
     }
     
     override func tearDown() {
@@ -20,9 +35,18 @@ class NicoLiveUserTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test01_allocation() {
+		let singleUser:NicoLiveUser = NicoLiveUser(user: user!, identifier: "6347612", anonymous: false, lang: UserLanguage.en)
+		XCTAssertNotNil(singleUser, "Single user can not initialize")
+		XCTAssertNotNil(singleUser.name.nickname, "user nickname can not correct")
+		XCTAssertNotNil(singleUser.name.nickname, "user handle can not correct")
+		XCTAssertNotNil(singleUser.name.nickname, "user id can not correct")
+		XCTAssertEqual(singleUser.isPremium, true, "user is premium but class is not")
+		XCTAssertEqual(singleUser.friendship, Friendship.met, "user friendship incorrect")
+		XCTAssertEqual(singleUser.language, UserLanguage.en, "user Language incorrect")
+		XCTAssertFalse(singleUser.lock, "user isn’t lock but instance is locked")
+		XCTAssertNil(singleUser.color, "user haven’t color but instance have color")
+		XCTAssertNil(singleUser.note, "user haven’t note but instance have note")
     }
 
     func testPerformanceExample() {
