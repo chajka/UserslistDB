a4d9fee0fece32ec06efa6ef6de1d9e10c07741c
Чайка <mellanie335@gmail.com>
2018-07-05 20:33:24 +0900

    Add empty class NicoLiveListeners and test files.

---
+++ UserslistDB/UsersManagement/NicoLiveListeners.swift
@@ -0,0 +1,13 @@
+//
+//  NicoLiveListeners.swift
+//  UserslistDB
+//
+//  Created by Чайка on 2018/07/05.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class NicoLiveListeners: NSObject {
+
+}
