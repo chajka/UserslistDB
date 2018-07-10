d41f4eb1ced9cb976aaa3db9b08cb4e6326e38d1
Чайка <mellanie335@gmail.com>
2018-07-10 19:35:36 +0900

    Add class DatabaseConverter and tis tests file.

---
+++ UserslistDB/ConvertDatabase/DatabaseConverter.swift
@@ -0,0 +1,13 @@
+//
+//  DatabaseConverter.swift
+//  UserslistDB
+//
+//  Created by Чайка on 2018/07/10.
+//  Copyright © 2018 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+class DatabaseConverter: NSObject {
+
+}
