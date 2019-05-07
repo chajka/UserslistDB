a3a83449dad4986669549e2c6612028c965676e3
Я Чайка <mellanie335@gmail.com>
2019-05-08 05:01:09 +0900

    Create class JSONizableUsers for each user entry of userlist.json. Test JSONizableUsers success.

---
+++ UserslistDB/JSON Handling/JSONizableUsers.swift
@@ -0,0 +1,44 @@
+//
+//  JSONizableUsers.swift
+//  Charleston
+//
+//  Created by Я Чайка on 2019/05/07.
+//  Copyright © 2019 Чайка. All rights reserved.
+//
+
+import Cocoa
+
+public final class JSONizableUsers: NSObject, Codable {
+		// MARK: - Properties
+	public var anonymous: Bool
+	public var monitor: Bool
+
+		// MARK: - Member variables
+	private var listener: Dictionary<String, JSONizableUser>
+
+		// MARK: - Constructor/Destructor
+	public convenience override init () {
+		self.init(anonymousComment: false, enableMoonitor: false)
+	}// end convenience init
+
+	public init (anonymousComment anonymous: Bool, enableMoonitor monitor: Bool) {
+		self.anonymous = anonymous
+		self.monitor = monitor
+		listener = Dictionary()
+	}// end init
+
+		// MARK: - Override
+		// MARK: - Actions
+		// MARK: - Public methods
+	public func user (identifier id: String) -> JSONizableUser? {
+		return listener[id]
+	}// end func user of
+
+	public func addUser (identifier id: String, with user: JSONizableUser)  {
+		listener[id] = user
+	}// end func id
+
+		// MARK: - Internal methods
+		// MARK: - Private methods
+		// MARK: - Delegates
+}// end class Users
