4a58de063bf32e364acb8e16defa7bcc3b072a6c
Я Чайка <mellanie335@gmail.com>
2019-06-05 20:49:16 +0900

    Initialize officialEntry/User and cruiseEntry/User in constructor.

--- UserslistDB/UsersManagement/Userslist.swift
+++ UserslistDB/UsersManagement/Userslist.swift
@@ -108,6 +108,10 @@ public final class Userslist: NSObject {
 			allUsers = JSONizableAllUsers()
 		}// end try - catch open data and parse json to dictionary
 		encoder.outputFormatting = JSONEncoder.OutputFormatting.prettyPrinted
+		officialEntry = JSONizableUser("Official", true, true)
+		officialUser = NicoLiveUser(user: officialEntry, identifier: officialEntry.handle, nickname: officialEntry.handle, premium: 0, anonymous: false, lang: UserLanguage.ja)
+		cruiseEntry = JSONizableUser("Cruise", true, true)
+		cruiseUser = NicoLiveUser(user: cruiseEntry, identifier: cruiseEntry.handle, nickname: cruiseEntry.handle, premium: 0, anonymous: false, lang: UserLanguage.ja)
 		super.init()
 		cleanupOutdatedUser()
 	}// end init
