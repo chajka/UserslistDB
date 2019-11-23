6fdfb062ec122c2ab847035a552e2a0d2995ba80
Я Чайка <mellanie335@gmail.com>
2019-11-24 01:54:28 +0900

    Pass user is new created or exist flag to constructor.

--- UserslistDB/UsersManagement/NicoLiveListeners.swift
+++ UserslistDB/UsersManagement/NicoLiveListeners.swift
@@ -83,8 +83,8 @@ public final class NicoLiveListeners: NSObject {
 		} else if identifier == cruiseUserIdentifier { nickname = cruiseUserName }
 		else if identifier == informationUserIdentifier { nickname = informationUserName }
 
-		let user: NicoLiveUser = NicoLiveUser(user: usr, identifier: identifier, nickname: nickname, premium: premium, anonymous: anonymous, lang: lang)
 		let userForIdentifier: (user: JSONizableUser, known: Bool) = knownUsers.user(identifier: identifier)
+		let user: NicoLiveUser = NicoLiveUser(user: userForIdentifier.user, known: userForIdentifier.known, identifier: identifier, nickname: nickname, premium: premium, anonymous: anonymous, lang: lang)
 		parse(user: user, id: identifier, premium: premium)
 
 		if identifier == cruiseUserIdentifier {
